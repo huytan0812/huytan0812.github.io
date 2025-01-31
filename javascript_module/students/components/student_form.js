e3404cdc91cc3d10381d0d014224d379760604ae
huytan0812 <huytan12c6@gmail.com>
2025-01-31 14:38:29 +0700

    Bug in event listener for new update btn

---
+++ javascript_module/students/components/student_form.js
@@ -0,0 +1,55 @@
+function StudentFormComponent() {
+    return `
+    <p class="form-title" id="student-form-title">
+        Thêm học sinh
+    </p>
+    <div class="form-group">
+        <label for="student-name">
+            <strong>Tên học sinh</strong>
+        </label>
+        <input type="text" class="form-control" id="student-name" name="name" placeholder="Tên học sinh">
+    </div>
+    <div class="form-check mt-3">
+        <p>
+            <strong>Giới tính</strong>
+        </p>
+        <p>
+            <input class="form-check-input" type="radio" name="gender" id="male" value="Nam">
+            <label class="form-check-label ms-2" for="male">
+            Nam
+            </label>
+        </p>
+        <p>
+            <input class="form-check-input" type="radio" name="gender" id="female" value="Nữ">
+            <label class="form-check-label ms-2" for="female">
+            Nữ
+            </label>
+        </p>
+    </div>
+    <div class="form-group mt-3">
+        <label for="math-score">
+            <strong>Điểm toán</strong>
+        </label>
+        <input type="number" class="form-control" id="math-score" name="math_score" placeholder="Điểm toán">
+    </div>
+    <div class="form-group mt-3">
+        <label for="english-score">
+            <strong>Điểm Anh</strong>
+        </label>
+        <input type="number" class="form-control" id="english-score" name="english_score" placeholder="Điểm Anh">
+    </div>
+    <div class="form-group mt-3">
+        <label for="literature-score">
+            <strong>Điểm văn</strong>
+        </label>
+        <input type="number" class="form-control" id="literature-score" name="literature_score" placeholder="Điểm văn">
+    </div>
+    <button type="submit" class="btn btn-primary mt-3" id="add-student-btn">Thêm học sinh</button>
+    <p id="update-btns" class="mt-3">
+        <button type="button" class="btn btn-primary" id="update-student-btn">Cập nhật</button>
+        <button type="button" class="btn btn-outline-secondary" id="cancel-btn">Huỷ</button>
+    </p>
+    `;
+}
+
+export { StudentFormComponent };
--- javascript_module/students/index.js
+++ javascript_module/students/index.js
@@ -1,10 +1,17 @@
+import { StudentFormComponent } from './components/student_form.js';
+
 document.addEventListener("DOMContentLoaded", function index() {
+    // Build Student Form
+    buildStudentForm();
+
     // Display all Student
     renderAllStudent();
 
     const studentForm = document.forms["student-form"];
     const updateBtns = document.querySelectorAll(".update-btn");
     const deleteBtns = document.getElementsByClassName("delete-btn");
+
+    const addStudentBtn = document.getElementById('add-student-btn');
 
     studentForm.addEventListener("submit", (event) => addStudent(event, studentForm));
 
@@ -14,40 +21,27 @@ document.addEventListener("DOMContentLoaded", function index() {
                 // Get student id from data attribute
                 const studentId = parseInt(this.dataset.updateStudent);
 
-                // Get all students from the localStorage
-                const localStudents = JSON.parse(localStorage.getItem("students"));
+                // Get current Student info
+                const currentStudentInfo = getStudentInfo(studentId);
 
-                // Get the current student uisng studentId
-                const getStudent = localStudents[`student_${studentId}`];
-                
-                // Rebuild the Student Form
-                const studentFormTitle = document.getElementById('student-form-title');
-                studentFormTitle.innerHTML = `Thay đổi thông tin học sinh: ${getStudent['name']}`;
+                // Build update Student Form
+                updateStudentForm(currentStudentInfo, studentForm);
 
-                const studentFormInputs = studentForm.querySelectorAll("input");
+                const updateStudentBtn = document.getElementById('update-student-btn');
+                const cancelBtn = document.getElementById('cancel-btn');
 
-                studentFormInputs.forEach(
-                    (input) => {
-                        if (input.name == 'gender') {
-                            if (input.value == getStudent['gender']) {
-                                input.checked = true;
-                            }
-                        }
-                        else {
-                            input.value = getStudent[input.name];
-                        }
-                    }
-                )
-
-                const addStudentBtn = document.getElementById('add-student-btn');
-                addStudentBtn.style.display = 'none';
-                const updateBtns = document.getElementById('update-btns');
-                updateBtns.style.display = 'block';
+                updateStudentBtn.addEventListener("click", updateStudentLS);
+                cancelBtn.addEventListener("click", buildStudentForm);
             })
         }
     )
 
 })
+
+function buildStudentForm() {
+    const studentFormComponent = document.getElementsByTagName('studentformcomponent')[0];
+    studentFormComponent.innerHTML = StudentFormComponent();
+}
 
 function renderAllStudent() {
     // Render all students from local storage
@@ -100,6 +94,8 @@ function studentRow(count, student) {
 
 function addStudent(event, studentForm) {
     // Add a new student form the form
+
+    // Prevent the from from submiting
     event.preventDefault();
 
     const students = document.getElementById('students-row');
@@ -133,4 +129,48 @@ function addStudent(event, studentForm) {
     
     // Append new student row
     students.append(studentRow(studentCount + 1, newStudent));
+
+    // Add new Event Listener for new update Student btn
+    const newUpdateStudentBtn = document.getElementsByClassName('update-btn')[studentCount];
+    newUpdateStudentBtn.addEventListener("click", updateStudentCb);
+}
+
+function getStudentInfo(studentId) {
+    // Get all students from the localStorage
+    const localStudents = JSON.parse(localStorage.getItem("students"));
+
+    // Get the current student uisng studentId
+    const studentInfo = localStudents[`student_${studentId}`];
+    
+    return studentInfo;
+}
+
+function updateStudentForm(studentInfo, studentForm) {
+    // Rebuild the Student Form
+    const studentFormTitle = document.getElementById('student-form-title');
+    studentFormTitle.innerHTML = `Thay đổi thông tin học sinh: ${studentInfo['name']}`;
+
+    const studentFormInputs = studentForm.querySelectorAll("input");
+
+    studentFormInputs.forEach(
+        (input) => {
+            if (input.name == 'gender') {
+                if (input.value == studentInfo['gender']) {
+                    input.checked = true;
+                }
+            }
+            else {
+                input.value = studentInfo[input.name];
+            }
+        }
+    )
+
+    const addStudentBtn = document.getElementById('add-student-btn');
+    addStudentBtn.style.display = 'none';
+    const updateBtns = document.getElementById('update-btns');
+    updateBtns.style.display = 'block';
+}
+
+function updateStudentLS() {
+    console.log("Update Student to LS");
 }
