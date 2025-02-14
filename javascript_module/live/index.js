5275671ed263cdccacd4e13911e6b1966e901ab0
Huy Tan <huytan12c6@gmail.com>
2025-02-14 11:31:01 +0700

    Setup

--- javascript_module/live/index.js
+++ javascript_module/live/index.js
@@ -2,6 +2,27 @@ import { MenuComponent } from "./components/menu_component.js";
 import { LeftSidebarComponent } from "./components/left_sidebar_component.js";
 import { RightSidebarComponent } from "./components/right_sidebar_component.js";
 
+const fruitsArr = [
+    {
+        'kiwi': {
+            'name': "kiwi",
+            'description': "Kiwi là trái cây màu xanh"
+        }
+    },
+    {
+        'mango': {
+            'name': "mango",
+            'description': "Xoài là trái cây nhiệt đới"
+        }
+    },
+    {
+        'dragon_fruit': {
+            'name': "dragon fruit",
+            'description': "Thanh long là trái cây nhiệt đới"
+        }
+    }
+]
+
 document.addEventListener("DOMContentLoaded", function() {
     const menuComponent = document.getElementsByTagName('menucomponent')[0];
     const leftSidebarComponent = document.getElementsByTagName('leftsidebar')[0];
@@ -10,4 +31,15 @@ document.addEventListener("DOMContentLoaded", function() {
     menuComponent.innerHTML = MenuComponent();
     leftSidebarComponent.innerHTML = LeftSidebarComponent();
     rightSidebarComponent.innerHTML = RightSidebarComponent();
+
+    const newFruit = {
+        'banana': {
+            'name': 'banana',
+            'description': "Chuối là trái cây nhiệt đới"
+        }
+    }
+
+    fruitsArr.unshift(newFruit);
+    localStorage.setItem('fruits', JSON.stringify(fruitsArr));
 })
+
